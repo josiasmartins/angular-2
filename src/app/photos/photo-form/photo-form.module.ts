f123c17992a6cd245db75da0f4c755ad30a55cbc
Caitano <josiasmartinscaitano@gmail.com>
2022-01-06 22:07:59 -0300

    refatorado o componente photo-form mas com erro no template

--- src/app/photos/photo-form/photo-form.module.ts
+++ src/app/photos/photo-form/photo-form.module.ts
@@ -1,9 +1,15 @@
+import { VMessagesMoudule } from './../../shared/components/vmessages/vmessages.module';
+import { ControlContainer, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { PhotoFormComponent } from './photo-form.component';
 import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [PhotoFormComponent],
-    imports: [ CommonModule ]
+    imports: [
+      CommonModule,
+      ReactiveFormsModule,
+      VMessagesMoudule,
+     ],
 })
 export class PhotoFormModule { }
